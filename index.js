f81ca5404a7427f2b58af612fca679ef30075403
apl09 <adrianpastorlopez09@gmail.com>
2023-05-29 22:01:24 +0200

    bikram array funciones

---
+++ index.js
@@ -0,0 +1,35 @@
+const arrayVacio = [];
+
+const  arrayNumeros = [0,1,2,3,4,5,6,7,8,9];
+
+const  arrayNumerosPares = [0,2,4,6,8];
+
+const arrayBidimensional =  [[0, 1, 2], ['a', 'b', 'c']];
+
+const suma = (a, b) => {
+    return a + b;
+};
+
+const potenciacion = (a, b) => {
+    return a**b;
+};
+
+const separarPalabras = (string) => {    
+    return string.split(" ");
+};
+
+const repetirString = (string,num) => {
+    for(let numero of num) {
+        return numero + string;
+    }
+
+} 
+
+ const esPrimo = (Num) => {
+    if( num )
+} 
+
+
+
+
+
